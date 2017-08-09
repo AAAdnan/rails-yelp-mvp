9348f9b28b297d3c3e5fc7b4361a91b1a912d638
Adnan Ahmed <adnan.ahmed01@gmail.com>
2017-08-09 12:17:54 +0100

    model done

---
+++ db/migrate/20170809110133_create_reviews.rb
@@ -0,0 +1,10 @@
+class CreateReviews < ActiveRecord::Migration[5.0]
+  def change
+    create_table :reviews do |t|
+      t.string :content
+      t.integer :rating
+      t.string :restaurant_id
+      t.timestamps
+    end
+  end
+end
